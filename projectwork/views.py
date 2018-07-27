ae329f10e66baccf89229f0b00510d8a6ab1de4c
MosesAyo <Legendmoses360@yahoo.com>
2018-07-27 02:13:40 +0100

    Second commit

--- projectwork/views.py
+++ projectwork/views.py
@@ -1,31 +1,44 @@
-from django.utils.html import format_html
+# Views.py controls what is being seen in the browser
 from django.http import HttpResponse 
 from django.shortcuts import render
 from .forms import urlForm
 import requests
+from bs4 import BeautifulSoup
 
 def home_page(request):
-    form = urlForm(request.POST or None)
-    context ={
-        "form": form
-    }
     if request.method == 'POST':
         search_id = request.POST.get('url', None)
         print (search_id)
+        def printurl():  
+            read = requests.get(search_id)
+            soup = BeautifulSoup(read.content)
+            #print(soup.find_all("a"))
+            for link in soup.find_all("a"):
+                printout = link.get("href")
+                print (printout)
+                
+            pass
+            
+        form = urlForm(request.POST or None)
+        context ={
+            "form": form,
+        }
+
         if form.is_valid():
+
             print("The form is valid")
-            response = requests.get(search_id + "%27").text
-
-            if 'You have an error in your SQL syntax;' in response: 
+            response = requests.get(search_id ).text
+            if 'You have an error in your SQL syntax;' in response:
                 print ('its vulnerable')
-
+                printurl()
+               
                 return render(request, "vulnerable.html", context)
                 
             else:
                 print ('its not vulnerable')
-                
-                return render(request,"notvulnerable.html", context)
-   
+                printurl()
 
-        
+                return render(request, "notvulnerable.html", context)
+   
+    
     return render(request, "index.html", context)
